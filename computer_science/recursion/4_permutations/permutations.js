5a830d78bd9bc343ec13ea6da24886f52cddb89e
Daria Solomkina <127635866+dasha-solomkina@users.noreply.github.com>
2026-09-16 12:18:22 -0300

    solve pascal and perms

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,42 +1,17 @@
-const permutations = function(inputArray) {
-    let answer = []
-    
-    if (inputArray.length === 0) {
-        answer.push([])
-    } 
-
-    for (let i = 0; i < inputArray.length; i++) {
-        const value = inputArray[i]
-        const arr = [...inputArray.slice(0, i), ...inputArray.slice(i + 1, inputArray.length)]
-        answer.push([value, ...arr])
-        const firstVari = [value, ...arr]
-        console.log("[value, ...arr", firstVari)
-        for (let j = 1; j < arr.length + 1; j++){
-            // console.log({arr})
-            // console.log({value})
-            // const first = arr.slice(0, j)
-            // const second = arr.slice(j)
-            // console.log({first})
-            // console.log({second})
-
-            const finalArray = [...arr.slice(0, j), value,  ...arr.slice(j) ]
-            console.log({finalArray})
-            answer.push(finalArray)
+const permutations = function(inputArray, perm = [], perms = []) {
+    if (!inputArray.length) {
+        perms.push(perm)
+    } else {
+        for (let i = 0; i < inputArray.length; i++) {
+            const newArray = [...inputArray.slice(0, i), ...inputArray.slice(i + 1)]
+            const newPerm = perm.concat(inputArray[i])
+            permutations(newArray, newPerm, perms)
         }
-
     }
-
-    const uniqueArrays = Array.from(
-    new Set(answer.map((subArr) => JSON.stringify(subArr))),
-    (str) => JSON.parse(str)
-    )
-
-    return uniqueArrays
+    return perms
 };
 
 const sample = [1, 2, 3]
-// const sample = []
-// const sample = [1]
 console.log('permutations', permutations(sample))
 
 // Do not edit below this line
--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,4 +1,22 @@
-const pascal = function () {};
+const pascal = function(n) {
+
+    if (n <= 1) return [1]
+
+    const nRow = []
+
+    const arrayBefore = pascal(n - 1)
+
+    const arrayForCalc = [...arrayBefore]
+    arrayForCalc.push(0)
+    arrayForCalc.unshift(0)
+
+    for (let i = 0; i < arrayForCalc.length - 1; i++) {
+        nRow.push(arrayForCalc[i] + arrayForCalc[i + 1])
+    }
+    
+    return nRow
+};
+
 
 // Do not edit below this line
 module.exports = pascal;
